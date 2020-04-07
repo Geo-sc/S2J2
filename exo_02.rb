0603fae450ec88802a4dbf42bd2cc865ecf18883
Geo-sc <geosc.dev@gmail.com>
2020-04-07 13:02:17 +0200

    modif

--- exo_02.rb
+++ exo_02.rb
@@ -3,3 +3,12 @@ puts "Bonjour, monde !"
 a = "Bonjour, monde !"
 
 puts "Et avec une voix sexy, ça donne :" + a 
+
+puts "Différence entre Puts et Print :"
+puts "Puts crée une nouvelle ligne"
+
+print "Différence entre Puts et Print :"
+print "Print ne crée pas de nouvelle ligne et laisse tout sur la même"
+
+print "\n" #print \n renvoi à la ligne lors d'un print
+puts ""  #renvoi à la ligne de base et puts "" (vide) correspond à print "\n"
---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, monde !"
+#a = "Bonjour, monde !"
+#Le programme ne s'éxécute plus parce que # permet d'écrire un commentaire.
+
+puts "Et avec une voix sexy, ça donne :" + a
